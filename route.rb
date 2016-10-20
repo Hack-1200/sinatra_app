8fdb32e78d02cd64f3df426466472b39eec1eb88
Hack-1200 <giyaz96@mail.ru>
2016-10-20 19:17:21 +0200

    be good

--- route.rb
+++ route.rb
@@ -7,14 +7,14 @@ require './server'
 # set :database, "sqlite3:ege_db.sqlite3"
 
 get '/' do 
-	# return_message={}
-	# serv=Server.all
-	# if !serv.empty?
-	# 	return_message[:status]="Success"
-	# 	# return_message=serv
-	# # else
-	# 	return_message[:status]="Error"
-	# # end
-	# return_message.to_json
-	"Hello"
+	return_message={}
+	serv=Server.all
+	if !serv.empty?
+		return_message[:status]="Success"
+		return_message=serv
+	else
+		return_message[:status]="Error"
+	end
+	return_message.to_json
+	# "Hello"
 end
